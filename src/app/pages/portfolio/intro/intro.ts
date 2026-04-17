e37ba2dbd7338048cd05433db6dfa7322e891599
Andrew Vinicius <74573141+andrewvininius@users.noreply.github.com>
2026-04-16 21:35:30 -0300

    ✨ feat: Criando os componetes da main

--- src/app/pages/portfolio/intro/intro.ts
+++ src/app/pages/portfolio/intro/intro.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-intro',
+//   imports: [],
+//   templateUrl: './intro.html',
+//   styleUrl: './intro.scss',
+// })
+// export class Intro {}
+import { Component, signal, effect } from '@angular/core';
 
 @Component({
   selector: 'app-intro',
-  imports: [],
+  standalone: true,
   templateUrl: './intro.html',
   styleUrl: './intro.scss',
 })
-export class Intro {}
+export class Intro {
+
+  isOpen = signal(false);
+
+  constructor() {
+    effect(() => {
+      document.body.style.overflow = this.isOpen() ? 'hidden' : 'auto';
+    });
+  }
+
+  toggleMenu() {
+    this.isOpen.update(v => !v);
+  }
+
+  closeMenu() {
+    this.isOpen.set(false);
+  }
+}
--- src/app/pages/portfolio/portfolio.ts
+++ src/app/pages/portfolio/portfolio.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { Intro } from "./intro/intro";
+import { AboutMe } from "./about-me/about-me";
+import { Skills } from "./skills/skills";
+import { Education } from "./education/education";
 
 @Component({
   selector: 'app-portfolio',
-  imports: [Intro],
+  imports: [Intro, AboutMe, Skills, Education],
   templateUrl: './portfolio.html',
   styleUrl: './portfolio.scss',
 })
